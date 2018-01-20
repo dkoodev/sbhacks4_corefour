34cbfa06bb8e453b045e7d8558d8e5c1615ef4cc
Daniel Koo <koodanie@usc.edu>
2018-01-20 00:02:55 -0800

    firebase instead of express?

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+//
+// app.get('/', (req, res) => res.send('Hello World!'))
+//
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var firebase = require("firebase");
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "<API_KEY>",
+  authDomain: "<PROJECT_ID>.firebaseapp.com",
+  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+};
+firebase.initializeApp(config);
