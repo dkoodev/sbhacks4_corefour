b4456e1ccb4b7e37a791292c44f1dad7f2768ab0
Nishir <nshelat@usc.edu>
2018-01-20 01:05:44 -0800

    Added admin stuff

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ var config = {
   storageBucket: "",
 };
 firebase.initializeApp(config);
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://sbhacks-corefour.firebaseio.com"
+});
+
+// Start Admin stuff
+var admin = require("firebase-admin");
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("sbhacks-corefour-firebase-adminsdk-qict0-73374226da.json");
+
+// Initialize the app with a null auth variable, limiting the server's access
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://databaseName.firebaseio.com",
+  databaseAuthVariableOverride: null
+});
+
+// The app only has access to public data as defined in the Security Rules
+var db = admin.database();
+var ref = db.ref("/public_resource");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
